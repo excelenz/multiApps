031575cd1bfbe5e94de8ebe8a17597804f4b5577
excelenz <margolin@visart.biz>
2021-05-13 15:44:41 +0300

    third'

--- customers/middleware.py
+++ customers/middleware.py
@@ -1,5 +1,8 @@
 from . models import Customer
 
+#http://127.0.0.1:8000/admin/customers/customer/add/ adding new one and getting id data from server
+#http://127.0.0.1:8000/admin/customers/customer/ list
+#http://127.0.0.1:8000/admin/customers/customer/2/change/ getting one
 class faces:
     def __init__(self, get_response):
         self.get_response = get_response

b8c9d54eda4bf54680d5b24687ecb8965e321064
excelenz <margolin.dm@gmail.com>
2021-05-14 03:33:36 +0300

    pushing to main using bush file

--- customers/middleware.py
+++ customers/middleware.py
@@ -26,18 +26,22 @@ class faces:
                 if e:
                     print("user {}" .format(e))
                     customer_name = Customer.objects.get(id_customer=e)
-                    print(customer_name)
-                    print(self.call_to_biostar2(e))
+                    print("new name {}".format(customer_name))
+                    payload={'id_customer':e,'customer_name':customer_name}
+                    self.call_to_biostar2(**payload)
+
+
             elif request.method == 'POST':
                 print(request.POST.get('url_name'))
-                if 'customers_customer_add' in request.POST.get('url_name'):
-                    print("YYYYESS")
+                if request.POST.get('url_name'):
+                    if 'customers_customer_add' in request.POST.get('url_name'):
+                        print("YYYYESS")
                 # /admin/customers/customer/add/
                 #<QueryDict: {'csrfmiddlewaretoken': ['JGuf8KOFRr0GuifcnWawUAM6t7djm5s5hF7qizzUSQAwQ1m3j5xmJQCVjHaYNZcQ'], 'name': ['newnew'], '_save': ['Save']}>
                 #<QueryDict: {'csrfmiddlewaretoken': ['2HDTnIV3dJYmL9dG5BjDBhvl8mmDmPn5AGg4xxGie8yc7Skx1KGtqxlaYWjiNJ7Q'], 'action': ['delete_selected'], 'select_across': ['0'], 'index': ['0'], '_selected_action': ['11', '10', '9', '8', '7']}>
                 print(request.content_params)
                 print(request.POST)
-                #print(request.__dict__)
+                print(request.__dict__)
                 print(request)
                 print(request.POST.get('name'))
                 #http://127.0.0.1:8000/admin/customers/customer/ list
@@ -52,7 +56,13 @@ class faces:
             request.Customer.save()
         return response
 
-    def call_to_biostar2(self,id_user):
-        url="http://127.0.0.1:8000/?id_user={}".format(id_user)
+    def call_to_biostar2(self,**kwargs):
+        for key in ('id_customer','customer_name'):
+            try:
+                setattr(self, key, kwargs[key])
+            except:
+                setattr(self, key, "")
+        url="http://127.0.0.1:8000/?id_user={}".format(self.id_customer)
         newuser = requests.get(url)
-        return """[{ "access_groups": [ { "id": 0, "included_by_user_group": "string", "name": "string" } ], "card_count": 0, "email": "string", "expiry_datetime": "string", "face_template_count": 0, "fingerprint_template_count": 0, "last_modify": 0, "login_id": "string", "name": "string", "password_exist": true, "permission": { "id": 0, "name": "string", "permissions": [ { "allowed_group_id_list": [ "long" ], "module": "string", "read": true, "write": true } ] }, "phone_number": "string", "photo": "string", "pin_exist": true, "security_level": "string", "start_datetime": "string", "status": "string", "user_group": { "id": 0, "name": "string" }, "user_id": "string" }]"""
+        a=  """[{ "access_groups": [ { "id": 0, "included_by_user_group": "string", "name": "string" } ], "card_count": 0, "email": "string", "expiry_datetime": "string", "face_template_count": 0, "fingerprint_template_count": 0, "last_modify": 0, "login_id": "string", "name": "string", "password_exist": true, "permission": { "id": 0, "name": "string", "permissions": [ { "allowed_group_id_list": [ "long" ], "module": "string", "read": true, "write": true } ] }, "phone_number": "string", "photo": "string", "pin_exist": true, "security_level": "string", "start_datetime": "string", "status": "string", "user_group": { "id": 0, "name": "string" }, "user_id": "string" }]"""
+        return 1

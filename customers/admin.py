7322b091aa2006f216be5e422dd06dda89208937
excelenz <margolin.dm@gmail.com>
2021-05-14 06:04:03 +0300

    pushing to main using bush file

--- customers/admin.py
+++ customers/admin.py
@@ -1,18 +1,19 @@
 from django.contrib import admin
 from .models import Customer
-from django.contrib import admin
+import random
 
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-
-    list_display = ("name", "id_customer","created_on")
+    list_display = ("name", "id_customer","created_on","show_biostar2")
 
     class Meta:
         ordering = ("created_on", "name")
 
-    def show_unchangable_data_from_biostar2(self, obj):
-        result = Customer.objects.filter(id_customer=obj)
-        return "ddd"
+    def show_biostar2(self, obj): #show_unchangable_data_from_biostar2
+        print(obj)
+        result = Customer.objects.get(name=obj)
+        return str(random.randrange(100000, 1000000)) + "user_id" + result.id_customer
+    show_biostar2.short_description = "Unchangeable data"
 
 #admin.site.register(Customer)

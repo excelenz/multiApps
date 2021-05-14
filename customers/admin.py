fdf725134a5e4077640ff85ab38ab6cc2113d62d
excelenz <margolin.dm@gmail.com>
2021-05-14 05:29:54 +0300

    pushing to main using bush file

--- customers/admin.py
+++ customers/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import Customer
+from django.contrib import admin
 
-admin.site.register(Customer)
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+
+    list_display = ("name", "id_customer","created_on")
+
+    class Meta:
+        ordering = ("created_on", "name")
+
+    def show_unchangable_data_from_biostar2(self, obj):
+        result = Customer.objects.filter(id_customer=obj)
+        return "ddd"
+
+#admin.site.register(Customer)
--- customers/models.py
+++ customers/models.py
@@ -4,6 +4,7 @@ class Customer(models.Model):
     id_customer = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200, unique=True)
     created_on = models.DateTimeField(auto_now_add=True)
+    
 
     class Meta:
         ordering = ['-created_on']
